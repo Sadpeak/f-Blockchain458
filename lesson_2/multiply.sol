e66af3fe679aeafc4f5fcfcaa38be7889d27b2ce
Sadpeak <maxstack@mail.ru>
2021-10-16 22:29:33 +0300

    lesson 2

---
+++ lesson_2/multiply.sol
@@ -0,0 +1,26 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract multi {
+
+	uint public sum = 1;
+
+	constructor() public {
+		require(tvm.pubkey() != 0, 101);
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+	}
+
+	modifier checkOwnerAndAccept {
+
+		require(msg.pubkey() == tvm.pubkey(), 102);
+      
+		tvm.accept();
+		_;
+	}
+
+	function multiply(uint value) public checkOwnerAndAccept {
+      require(value >= 1 && value <= 10, 103);
+		sum *= value;
+	}
+}
